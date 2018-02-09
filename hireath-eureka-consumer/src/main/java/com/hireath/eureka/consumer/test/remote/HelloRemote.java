c7c42439adb334fae990fb84007c2333a7a87f2a
xun <xun@capitek.com.cn>
2018-02-09 17:16:52 +0800

    nothing important

--- hireath-eureka-consumer/src/main/java/com/hireath/eureka/consumer/test/remote/HelloRemote.java
+++ hireath-eureka-consumer/src/main/java/com/hireath/eureka/consumer/test/remote/HelloRemote.java
@@ -1,15 +1,13 @@
 package com.hireath.eureka.consumer.test.remote;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by summer on 2017/5/11.
  */
-@FeignClient(name= "spring-cloud-producer")
+@FeignClient(name= "spring-cloud-producer",fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
     @RequestMapping(value = "/hello")
